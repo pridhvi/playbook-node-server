d00ac0ff7a850b6fab2052f68f566723c370a36e
pridhvi <pridhvimuthuraju@gmail.com>
2023-04-03 07:32:38 -0400

    added type of search data

--- controllers/igdbController.js
+++ controllers/igdbController.js
@@ -10,11 +10,11 @@ const config = {
 };
 
 const getSearchCriteria = async (req, res) => {
-  const data = `search "${req.query.keyword}";
+  const data = `search "${req.query.criteria}";
     \nf alternative_name,character,checksum,collection,company,description,game,name,platform,published_at,test_dummy,theme;
+    \nw ${req.query.type}!=n;
     \nl ${req.query.pageSize};
     \no ${req.query.pageNumber};`;
-
   const searchData = await axios.post(`${IGDB_API_URL}/search`, data, config);
   return res.json(searchData.data);
 };
@@ -23,7 +23,7 @@ const getGameById = async (req, res) => {
   const data = `f age_ratings,aggregated_rating,aggregated_rating_count,alternative_names,artworks,bundles,category,checksum,collection,cover,created_at,dlcs,expanded_games,expansions,external_games,first_release_date,follows,forks,franchise,franchises,game_engines,game_localizations,game_modes,genres,hypes,involved_companies,keywords,language_supports,multiplayer_modes,name,parent_game,platforms,player_perspectives,ports,rating,rating_count,release_dates,remakes,remasters,screenshots,similar_games,slug,standalone_expansions,status,storyline,summary,tags,themes,total_rating,total_rating_count,updated_at,url,version_parent,version_title,videos,websites;where id = ${req.params.id};`;
 
   const game = await axios.post(`${IGDB_API_URL}/games`, data, config);
-//   console.log(game.data);
+  // console.log(req.params.id);
   return res.json(game.data);
 };
 
