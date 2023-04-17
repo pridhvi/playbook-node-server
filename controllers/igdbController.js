12a33ae76d3524ddd712f395fffaf53ed2ebb355
pridhvi <pridhvimuthuraju@gmail.com>
2023-04-16 23:14:52 -0400

    updated game fields

--- controllers/igdbController.js
+++ controllers/igdbController.js
@@ -27,7 +27,7 @@ const getSearchCriteria = async (req, res) => {
 
 const getGameById = async (req, res) => {
   try {
-    const data = `f age_ratings,aggregated_rating,aggregated_rating_count,alternative_names,artworks,bundles,category,checksum,collection,cover.url,created_at,dlcs,expanded_games,expansions,external_games,first_release_date,follows,forks,franchise,franchises,game_engines,game_localizations,game_modes,genres,hypes,involved_companies,keywords,language_supports,multiplayer_modes,name,parent_game,platforms.name,player_perspectives,ports,rating,rating_count,release_dates,remakes,remasters,screenshots,similar_games,slug,standalone_expansions,status,storyline,summary,tags,themes,total_rating,total_rating_count,updated_at,url,version_parent,version_title,videos,websites;where id = ${req.params.id};`;
+    const data = `f age_ratings,aggregated_rating,aggregated_rating_count,alternative_names,artworks.url,bundles,category,checksum,collection,cover.url,created_at,dlcs,expanded_games,expansions,external_games,first_release_date,follows,forks,franchise,franchises,game_engines,game_localizations,game_modes,genres,hypes,involved_companies,keywords,language_supports,multiplayer_modes,name,parent_game,platforms.name,player_perspectives,ports,rating,rating_count,release_dates,remakes,remasters,screenshots.url,similar_games,slug,standalone_expansions,status,storyline,summary,tags,themes,total_rating,total_rating_count,updated_at,url,version_parent,version_title,videos,websites.url;where id = ${req.params.id};`;
     const game = await axios.post(`${IGDB_API_URL}/games`, data, config);
     return res.json(game.data);
   } catch (error) {
